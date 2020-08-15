77f9d3f0bd87a5b8ae5d0f88f01ad2eab92cbefc
onion <62121953+cyanewfag@users.noreply.github.com>
2020-08-15 00:43:31 -0400

    Update onions framework.lua

--- onions framework.lua
+++ onions framework.lua
@@ -1,11 +1,13 @@
-local guiOpened = false;
 local guiInfo = { 100, 100, 250, 350, false, 0, 0 };
 local colors = { color.new(20, 20, 20, 255), color.new(30, 30, 30, 255), color.new(125, 125, 125, 255), color.new(40, 40, 40, 255), color.new(45, 45, 45, 255), color.new(255, 255, 255, 255), color.new(35, 35, 35, 255) };
 local fonts = { renderer.create_font("Verdana", 10, false), renderer.create_font("Verdana", 12, false) };
 local mousePos;
 local mouseDown = { false, 0, 0 };
+local keyArray = { {0x41, "A"}, {0x42, "B"}, {0x43, "C"}, {0x44, "D"}, {0x45, "E"}, {0x46, "F"}, {0x47, "G"}, {0x48, "H"}, {0x49, "I"}, {0x4A, "J"}, {0x4B, "K"}, {0x4C, "L"}, {0x4D, "M"}, {0x4E, "N"}, {0x4F, "O"}, {0x50, "P"}, {0x51, "Q"}, {0x52, "R"}, {0x53, "S"}, {0x54, "T"}, {0x55, "U"}, {0x56, "V"}, {0x57, "W"}, {0x58, "X"}, {0x59, "Y"}, {0x5A, "Z"} };
+local keyPressTable = {};
 
 local vars = {
+    { "onion_gui_enabled", true, 0x4D, "M", false },
     { "onion_gui_draggable", true },
     { "onion_hud_enabled", true },
     { "onion_hud_drag", false },
@@ -153,29 +155,42 @@ local function onionColorPicker(x, y, z, text, var)
     end
 end
 
+local function onionKeybind(x, y, text, var)
+    local enabled = checkVar(var);
+    local textSize = renderer.get_text_size(checkVar(var, 4), fonts[2]);
+
+    renderer.rect( x, y, textSize.x + 16, 18, colors[2]);
+    renderer.filled_rect( x + 1, y + 1, textSize.x + 14, 16, colors[5]);
+    renderer.rect( x + 3, y + 3, textSize.x + 10, 12, colors[2]);
+    renderer.filled_rect( x + 4, y + 4, textSize.x + 8, 10, colors[1]);
+
+    if (checkVar(var, 5)) then
+        if (#keyPressTable ~= 0) then
+            setVar(var, keyPressTable[1][1], 3);
+            setVar(var, keyPressTable[1][2], 4);
+            setVar(var, false, 5)
+        end
+    end
+
+    if (keys.key_pressed(checkVar(var, 3))) then
+        setVar(var, not checkVar(var))
+    end
+
+    if (keys.key_pressed(0x01)) then
+        if (mouseDown[1] and mouseDown[2] >= x and mouseDown[2] <= x + textSize.x + 16 and mouseDown[3] >= y and mouseDown[3] <= y + 18) then
+            setVar(var, true, 5)
+        end
+    end
+
+    if (checkVar(var, 5) or mousePos.x >= x and mousePos.x <= x + textSize.x + 16 and mousePos.y >= y and mousePos.y <= y + 18) then
+        renderer.filled_rect( x + 5, y + 5, textSize.x + 6, 8, colors[5]);
+    end
+
+    renderer.text(x + 8, y + ((18 / 2) - (textSize.y / 2)), checkVar(var, 4), colors[6], fonts[2]);
+end
+
 local function drawGUI()
     if (guiOpened) then
-        if (keys.key_down(0x01)) then
-            if (mouseDown[1] == false) then
-                mouseDown[1] = true;
-                mouseDown[2], mouseDown[3] = mousePos.x, mousePos.y;
-
-                if (mousePos.x >= guiInfo[1] and mousePos.x <= guiInfo[1] + guiInfo[3] and mousePos.y >= guiInfo[2] and mousePos.y <= guiInfo[2] + 20) then
-                    guiInfo[5] = true;
-                    guiInfo[6], guiInfo[7] = mousePos.x - guiInfo[1], mousePos.y - guiInfo[2];
-                end
-            elseif (guiInfo[5] and checkVar("onion_gui_draggable")) then
-                guiInfo[1], guiInfo[2] = mousePos.x - guiInfo[6], mousePos.y - guiInfo[7];
-            end
-        else
-            if (mouseDown[1]) then
-                mouseDown[1] = false;
-                mouseDown[2], mouseDown[3] = 0, 0;
-                guiInfo[5] = false;
-                guiInfo[6], guiInfo[7] = 0, 0;
-            end
-        end
-
         renderer.filled_rect(guiInfo[1], guiInfo[2], guiInfo[3], guiInfo[4], colors[1]);
         renderer.filled_rect(guiInfo[1], guiInfo[2], guiInfo[3], 20, colors[2]);
         renderer.rect(guiInfo[1], guiInfo[2], guiInfo[3], guiInfo[4], colors[3]);
@@ -185,6 +200,7 @@ local function drawGUI()
         onionCheckbox( guiInfo[1] + 10, guiInfo[2] + 54, 18, "Enable HUD", "onion_hud_enabled" );
         onionCheckbox( guiInfo[1] + 10, guiInfo[2] + 78, 18, "Draggable HUD", "onion_hud_drag" );
         onionColorPicker( guiInfo[1] + 10, guiInfo[2] + 102, 18, "HUD Color", "onion_hud_color");
+        onionKeybind(guiInfo[1] + 10, guiInfo[2] + 126, "peepee", "onion_gui_enabled")
     end
 end
 
@@ -199,13 +215,46 @@ local function drawHUD()
     end
 end
 
-function on_render()
-    if (keys.key_pressed(0x4D)) then
+local function inputHandler()
+    if (keys.key_pressed(checkVar("onion_gui_enabled", 3))) then
         guiOpened = not guiOpened;
     end
 
     mousePos = keys.get_mouse();
 
+    if (keys.key_down(0x01)) then
+        if (mouseDown[1] == false) then
+            mouseDown[1] = true;
+            mouseDown[2], mouseDown[3] = mousePos.x, mousePos.y;
+
+            if (mousePos.x >= guiInfo[1] and mousePos.x <= guiInfo[1] + guiInfo[3] and mousePos.y >= guiInfo[2] and mousePos.y <= guiInfo[2] + 20) then
+                guiInfo[5] = true;
+                guiInfo[6], guiInfo[7] = mousePos.x - guiInfo[1], mousePos.y - guiInfo[2];
+            end
+        elseif (guiInfo[5] and checkVar("onion_gui_draggable")) then
+            guiInfo[1], guiInfo[2] = mousePos.x - guiInfo[6], mousePos.y - guiInfo[7];
+        end
+    else
+        if (mouseDown[1]) then
+            mouseDown[1] = false;
+            mouseDown[2], mouseDown[3] = 0, 0;
+            guiInfo[5] = false;
+            guiInfo[6], guiInfo[7] = 0, 0;
+        end
+    end
+
+    keyPressTable = {};
+    for i = 1, #keyArray do
+        if (keyArray[i][1] ~= nil) then
+            if (keys.key_pressed(keyArray[i][1])) then
+                table.insert(keyPressTable, {keyArray[i][1], keyArray[i][2]});
+            end
+        end
+    end
+end
+
+function on_render()
+    inputHandler();
     drawGUI();
     drawHUD();
 end
